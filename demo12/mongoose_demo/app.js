21596215e220b8dcd189463132e1c9e371d5b4cf
josephsapinoso <josephsapinoso@gmail.com>
2016-08-19 13:34:29 -0700

    Mongoose Modeling
    Created a mongoose schema, model and user. Tested with mongo

---
+++ demo12/mongoose_demo/app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//---- Practice mongoose here----------------------------------------------------------
+
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+      firstname: 'string', 
+      lastname: 'string'
+    });
+
+var User = mongoose.model ('User', UserSchema); 
+// 'User' is the name of the model, and UserSchema is the name of the schema we will use
+
+var user1 = new User();
+user1.firstname = "Sandra";
+user1.lastname = "Williams";
+
+user1.save(function(err, user1){
+  if(err)
+    res.err;
+});
+
+mongoose.connect('mongodb://localhost/mydb');
+//--------------------------------------------------------------------------------------
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
